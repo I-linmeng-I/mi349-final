a16b41f01e00b4b461c4c3b67897ff030eeacc40
i-linmeng-i <1364876516@qq.com>
2022-12-13 03:26:14 +0900

    1

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
+var exalUrl = "http://127.0.0.1:5500/data/lianjin.xlsx"
+var dlc=5
+
+window.οnlοad=load();
+function load(){
+    exalToSheet()
+}
+
 function JobOnClick(job)
 {
-    
+    exalUrl = `http://127.0.0.1:5500/data/${job}.xlsx`;
+	
+    exalToSheet()
+}
 
-    var url = `http://127.0.0.1:5500/data/${job}.xlsx`;
-	let list = ''
+function DlcOnClick(a)
+{
+    dlc =a;
+	
+    exalToSheet()
+}
 
+function exalToSheet(){
+    let list = ''
 	/* set up async GET request */
 	var req = new XMLHttpRequest();
-	req.open("GET", url, true);
+	req.open("GET", exalUrl, true);
 	req.responseType = "arraybuffer";
 	req.onload = function(e) {
 	  var workbook = XLSX.read(req.response);
-		list = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
+		list = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[dlc]])
 		let list_dom = document.querySelector('.missionList')
 		list_dom.innerHTML = ''
         
         let item = document.createElement('tr')
 		item.className = 'item'
         item.innerHTML += `
-        <th class="box">等级</th>
-        <th class="box">理符名称</th>
-        <th class="box">提取位置</th>
-        <th class="box">经验</th>
-        <th class="box">奖励金币</th>
-        <th class="box">提交道具</th>`
+        <th class="box level">等级</th>
+        <th class="box name">理符名称</th>
+        <th class="box place">提交位置</th>
+        <th class="box exp">经验</th>
+        <th class="box gold">奖励金币</th>
+        <th class="box item">提交道具</th>
+        `
         list_dom.appendChild(item)
 
 
-		if(job != "duantie"){
+		if(exalUrl != `http://127.0.0.1:5500/data/duantie.xlsx`){
             list_dom.appendChild(addTH((list[0])))
 		    list.map(res=>{
 			    list_dom.appendChild(addTD(res))
@@ -84,12 +102,12 @@ function makeitem(data,dataData){
     }
     
 
-    item.innerHTML +=  `<td class="box">${level}</td>`
-    item.innerHTML +=  `<td class="box">${missionName}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[3]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[1]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[0]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[4]}</td>`
+    item.innerHTML +=  `<td class="box level">${level}</td>`
+    item.innerHTML +=  `<td class="box name">${missionName}</td>`
+    item.innerHTML +=  `<td class="box place">${dataData[3]}</td>`
+    item.innerHTML +=  `<td class="box exp">${dataData[1]}</td>`
+    item.innerHTML +=  `<td class="box gold">${dataData[0]}</td>`
+    item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
 
     return item
 }
@@ -118,12 +136,12 @@ function addTHZhujia(data){
     }
     
 
-    item.innerHTML +=  `<td class="box">${level}</td>`
-    item.innerHTML +=  `<td class="box">${missionName}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[1]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[2]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[3]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[4]}</td>`
+    item.innerHTML +=  `<td class="box level">${level}</td>`
+    item.innerHTML +=  `<td class="box name">${missionName}</td>`
+    item.innerHTML +=  `<td class="box place">${dataData[1]}</td>`
+    item.innerHTML +=  `<td class="box exp">${dataData[2]}</td>`
+    item.innerHTML +=  `<td class="box gold">${dataData[3]}</td>`
+    item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
 
     return item
 }
@@ -151,12 +169,13 @@ function addTDZhujia(data){
     }
     
 
-    item.innerHTML +=  `<td class="box">${level}</td>`
-    item.innerHTML +=  `<td class="box">${missionName}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[1]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[2]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[3]}</td>`
-    item.innerHTML +=  `<td class="box">${dataData[4]}</td>`
+    item.innerHTML +=  `<td class="box level">${level}</td>`
+    item.innerHTML +=  `<td class="box name">${missionName}</td>`
+    item.innerHTML +=  `<td class="box place">${dataData[1]}</td>`
+    item.innerHTML +=  `<td class="box exp">${dataData[2]}</td>`
+    item.innerHTML +=  `<td class="box gold">${dataData[3]}</td>`
+    item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
 
     return item
 }
+
