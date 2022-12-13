53e66d38c437ab5787c84c439bca2ab412881bdf
i-linmeng-i <1364876516@qq.com>
2022-12-14 01:18:18 +0900

    1

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ var dlc=5
 
 window.οnlοad=load();
 function load(){
+    
+    console.log(1)
+    let list_domm = document.querySelector('.selectList')
+	list_domm.innerHTML = ''
+        
+    let itemm = document.createElement('tr')
+	itemm.className = 'item'
+    itemm.innerHTML += `
+    <th class="box level">等级</th>
+    <th class="name">理符名称</th>
+    <th class="place">提交位置</th>
+    <th class="box item">提交道具</th>
+    <th class="box missionNum">数量</th>
+    `
+    list_domm.appendChild(itemm)
+    console.log(1)
     exalToSheet()
 }
 
@@ -29,6 +45,8 @@ function exalToSheet(){
 	req.onload = function(e) {
 	  var workbook = XLSX.read(req.response);
 		list = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[dlc]])
+
+
 		let list_dom = document.querySelector('.missionList')
 		list_dom.innerHTML = ''
         
@@ -36,8 +54,8 @@ function exalToSheet(){
 		item.className = 'item'
         item.innerHTML += `
         <th class="box level">等级</th>
-        <th class="box name">理符名称</th>
-        <th class="box place">提交位置</th>
+        <th class="name">理符名称</th>
+        <th class="place">提交位置</th>
         <th class="box exp">经验</th>
         <th class="box gold">奖励金币</th>
         <th class="box item">提交道具</th>
@@ -87,7 +105,32 @@ function addTD(data){
 function makeitem(data,dataData){
     let item = document.createElement('tr')
     item.className = 'item'
-    
+
+    item.addEventListener('click',function(e){
+        let item = document.createElement('tr')
+        item.className = 'item'
+
+        item.innerHTML +=  this.innerHTML
+
+        item.cells[3].remove()
+        item.cells[3].remove()
+
+
+        item.innerHTML += '<th class="box missionNum">1</th>'
+
+        let list_domm = document.querySelector('.selectList')
+
+        for(let i=0;i<list_domm.rows.length;i++){
+            if(list_domm.rows[i].cells[1].innerHTML == item.cells[1].innerHTML)
+            {
+                list_domm.rows[i].cells[4].innerHTML++
+                return
+            }
+        }
+
+        list_domm.appendChild(item)
+    },false)
+
     var level = ""
     var missionName = ""
     for(let i=0;i<dataData[2].length;i++){
@@ -103,8 +146,8 @@ function makeitem(data,dataData){
     
 
     item.innerHTML +=  `<td class="box level">${level}</td>`
-    item.innerHTML +=  `<td class="box name">${missionName}</td>`
-    item.innerHTML +=  `<td class="box place">${dataData[3]}</td>`
+    item.innerHTML +=  `<td class="name">${missionName}</td>`
+    item.innerHTML +=  `<td class="place">${dataData[3]}</td>`
     item.innerHTML +=  `<td class="box exp">${dataData[1]}</td>`
     item.innerHTML +=  `<td class="box gold">${dataData[0]}</td>`
     item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
@@ -137,8 +180,8 @@ function addTHZhujia(data){
     
 
     item.innerHTML +=  `<td class="box level">${level}</td>`
-    item.innerHTML +=  `<td class="box name">${missionName}</td>`
-    item.innerHTML +=  `<td class="box place">${dataData[1]}</td>`
+    item.innerHTML +=  `<td class="name">${missionName}</td>`
+    item.innerHTML +=  `<td class="place">${dataData[1]}</td>`
     item.innerHTML +=  `<td class="box exp">${dataData[2]}</td>`
     item.innerHTML +=  `<td class="box gold">${dataData[3]}</td>`
     item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
@@ -170,8 +213,8 @@ function addTDZhujia(data){
     
 
     item.innerHTML +=  `<td class="box level">${level}</td>`
-    item.innerHTML +=  `<td class="box name">${missionName}</td>`
-    item.innerHTML +=  `<td class="box place">${dataData[1]}</td>`
+    item.innerHTML +=  `<td class="name">${missionName}</td>`
+    item.innerHTML +=  `<td class="place">${dataData[1]}</td>`
     item.innerHTML +=  `<td class="box exp">${dataData[2]}</td>`
     item.innerHTML +=  `<td class="box gold">${dataData[3]}</td>`
     item.innerHTML +=  `<td class="box item">${dataData[4]}</td>`
